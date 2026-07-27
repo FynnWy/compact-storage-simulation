c973271eba8ff840481e217916546be8c2ca3136
FynnWy <Fynn.wy@googlemail.com>
2026-07-27 14:20:13 +0200

    Es funktioniert!!! (Roboter holt Kisten ab, lagert um, bringt zur Pickstation, holt an Pickstation ab)

--- events/event_types.py
+++ events/event_types.py
@@ -7,3 +7,7 @@ class EventType(Enum):
     PICKSTATION_COMPLETE = "pickstation_complete"
     REQUEST_COMPLETE = "request_complete"
     ROBOT_MOVE = "ROBOT_MOVE"
+
+    # NEU: Zwei-Phasen-Aktionen
+    ROBOT_PICKUP = "robot_pickup"  # Roboter nimmt Bin auf
+    ROBOT_DROP = "robot_drop"  # Roboter legt Bin ab
